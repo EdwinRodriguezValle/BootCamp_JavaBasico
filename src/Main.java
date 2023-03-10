3ff47a2c4ab6a844f6eecf26e8f275c9064589f6
[firstname <[valid-email]>
2023-03-10 17:11:22 +0100

    Ejercio numero 3

--- src/Main.java
+++ src/Main.java
@@ -10,35 +10,31 @@ public class Main {
 //        holaMundo();
 //
 //
-//        //Bucle foreach
-//         String [] nombres = {"Edwin", "Sofia", "Claudia", "Gilberto"};
-//        for (String resultadoNombres : nombres) {
-//            System.out.println(resultadoNombres);
-//        }
-//
-//
-//        System.out.println();
-//
-//        int suma = 0;
-//        int[] numeros = {1,2,3,4,5,6,7,8,9};
-//
-//        for (int resultadoNumeros: numeros) {
-//            suma += resultadoNumeros;
-//            System.out.println(resultadoNumeros);
-//        }
-//        System.out.println(" la suma es: " + suma);
-//
-//        // bucle while
-//
-//        int contador = 0;
-//
-//        while (contador < 10){
-//            contador++;
-//            if(contador == 5){
-//                continue;
-//            }
-//            System.out.println("El valor del contador " + contador);
-//
+        //Bucle foreach
+
+
+        System.out.println();
+
+        int suma = 0;
+        int[] numeros = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+
+        for (int resultadoNumeros : numeros) {
+            suma += resultadoNumeros;
+            System.out.println(resultadoNumeros);
+        }
+        System.out.println(" la suma es: " + suma);
+
+        // bucle while
+
+        int contador = 0;
+
+        while (contador < 10) {
+            contador++;
+            if (contador == 5) {
+                continue;
+            }
+            System.out.println("El valor del contador " + contador);
+
 //
 //        }
 //
@@ -49,5 +45,6 @@ public class Main {
 //
 //    private static void holaMundo() {
 //    }
+        }
     }
 }
